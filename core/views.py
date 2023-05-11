f9302c8aeae52e6856f991efaefae22d85018f6f
Abel <abelx3678@gmail.com>
2023-05-11 18:34:08 -0400

    sin-cuenta adaptando

--- core/views.py
+++ core/views.py
@@ -6,3 +6,24 @@ def mostrarIndex(request):
 
 def mostrarNosotros(request):
     return render(request, 'core/sin-cuenta/nosotros.html')
+
+def mostrarRegistro(request):
+    return render(request, 'core/sin-cuenta/registrarse.html')
+
+def mostrarIni_sesion(request):
+    return render(request, 'core/sin-cuenta/inicio-sesion.html')
+
+def mostrarOlv_contra(request):
+    return render(request, 'core/sin-cuenta/olvidar-contraseña.html')
+
+def mostrarPregunta(request):
+    return render(request, 'core/sin-cuenta/Pregunta.html')
+
+def mostrarProducto(request):
+    return render(request, 'core/sin-cuenta/Producto.html')
+
+def mostrarCategoria(request):
+    return render(request, 'core/sin-cuenta/Categoria.html')
+
+def mostrarNosotros(request):
+    return render(request, 'core/sin-cuenta/nosotros.html')
