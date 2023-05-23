096b686d2417ae35850c843d293b0761878955c2
Abel <abelx3678@gmail.com>
2023-05-23 11:32:45 -0400

    modelos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Detalle, Rol, Pregunta, Categoria, Usuario, Producto, Venta, Consulta
 # Register your models here.
+
+admin.site.register(Rol)
+admin.site.register(Pregunta)
+admin.site.register(Categoria)
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(Detalle)
+admin.site.register(Consulta)
--- core/models.py
+++ core/models.py
@@ -4,20 +4,28 @@ from django.db import models
 class Rol(models.Model):
     id_rol = models.IntegerField(primary_key=True, verbose_name='Solo dos posibles, 1 para cliente y 2 para admin')
     nombre_rol = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.nombre_rol
 
 class Pregunta(models.Model):
     id_pregunta = models.IntegerField(primary_key=True, verbose_name='De momento solo 1,2 y 3')
     nombre_pregunta = models.CharField(max_length=60)
+    def __str__(self) -> str:
+        return self.nombre_pregunta
 
 class Categoria(models.Model):
     id_categoria = models.IntegerField(primary_key=True, verbose_name='De momento solo 7')
     nombre_categoria = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.nombre_categoria
 
 class Consulta(models.Model):
     id_consulta = models.AutoField(primary_key=True)
     nombre_consultante = models.CharField(max_length=30)
     asunto_consulta = models.CharField(max_length=60)
     mensaje_consulta = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.asunto_consulta
 
 class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
@@ -33,6 +41,8 @@ class Usuario(models.Model):
     foto_usuario = models.ImageField(upload_to="usuarios")
     rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
     pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.rut + self.dvrut + ""
 
 
 class Producto(models.Model):
@@ -43,6 +53,8 @@ class Producto(models.Model):
     stock = models.IntegerField()
     foto_prod = models.ImageField(upload_to="productos")
     unidad_medida = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.nombre_prod
 
 class Venta(models.Model):
     id_venta = models.AutoField(primary_key=True)
@@ -52,6 +64,8 @@ class Venta(models.Model):
     total = models.IntegerField()
     carrito = models.BooleanField(verbose_name='0 para venta y 1 para carrito')
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.id_venta
 
 class Detalle(models.Model):
     id_detalle = models.AutoField(primary_key=True)
@@ -59,6 +73,8 @@ class Detalle(models.Model):
     subtotal = models.IntegerField()
     venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.id_detalle
 
 ###incremental: models.AutoField()###
 ###imagenes: foto = models.ImageField(upload_to="carpeta")###
