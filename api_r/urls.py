29300d93bd9009c218587d92797977abdc6476cf
Abel <abelx3678@gmail.com>
2023-06-06 11:56:47 -0400

    url para serializer detalle

--- api_r/urls.py
+++ api_r/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api_r.views import lista_consulta
+from api_r.views import lista_consulta, lista_detalle
 
 urlpatterns = [
-    path('lista_consulta', lista_consulta, name="lista_consulta")
+    path('lista_consulta', lista_consulta, name="lista_consulta"),
+    path('lista_detalle', lista_detalle, name="lista_detalle")
 ]
