772bf7674f951a323905f9820254827df7c23104
Abel <abelx3678@gmail.com>
2023-06-08 11:10:48 -0400

    agregados put y delete de la api

--- api_r/urls.py
+++ api_r/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from api_r.views import lista_consulta, lista_detalle
+from api_r.views import lista_consulta, lista_detalle, detalle_detalle, detalle_consulta
 
 urlpatterns = [
     path('lista_consulta', lista_consulta, name="lista_consulta"),
-    path('lista_detalle', lista_detalle, name="lista_detalle")
+    path('lista_detalle', lista_detalle, name="lista_detalle"),
+    path('detalle_detalle/<id>', detalle_detalle, name='detalle_detalle'),
+    path('detalle_consulta/<id>', detalle_consulta, name='detalle_consulta'),
 ]
--- api_r/views.py
+++ api_r/views.py
@@ -27,8 +27,32 @@ def lista_consulta(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
+@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_consulta(request, id):
+    try:
+        consulta = Consulta.objects.get(id_consulta = id)
+    except Consulta.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    if request.method == 'GET':
+        serializer = consultaSerializer(consulta)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = consultaSerializer(consulta, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        consulta.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def lista_detalle(request):
@@ -48,3 +72,27 @@ def lista_detalle(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_detalle(request, id):
+    try:
+        detalle = Detalle.objects.get(id_detalle = id)
+    except detalle.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = detalleSerializer(detalle)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = detalleSerializer(detalle, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        detalle.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
